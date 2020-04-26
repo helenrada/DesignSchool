2ebf08e793b7b5453aa7a248c912510cae096f12
Helen <h.radaykina@gmail.com>
2020-04-26 16:22:53 +0300

    correct

--- app/js/main.js
+++ app/js/main.js
@@ -29,39 +29,6 @@ $(function () {
     focusOnSelect: true,
   });
 
-  // var contentSection = $(".content-section");
-  // var navigation = $(".menu-indicator");
-  // navigation.on("click", ".ind", function(event) {
-  //   event.preventDefault();
-  //   smoothScroll($(this.hash));
-  // });
-
-  // $(window).on("scroll", function() {
-  //   updateNavigation();
-  // });
-
-  // function updateNavigation() {
-  //   contentSection.each(function() {
-  //     var sectionName = $(this).attr("id");
-  //     var navigationMatch = $('.menu-indicator a[href="#' + sectionName + '"]');
-  //     if (
-  //       $(this).offset().top - $(window).height() / 2 < $(window).scrollTop() &&
-  //       $(this).offset().top + $(this).height() - $(window).height() / 2 >
-  //         $(window).scrollTop()
-  //     ) {
-  //       navigationMatch.addClass("active-section");
-  //     } else {
-  //       navigationMatch.removeClass("active-section");
-  //     }
-  //   });
-  // }
-  // function smoothScroll(target) {
-  //   $("body,html").animate(
-  //     {
-  //       scrollTop: target.offset().top
-  //     },
-  //     800
-  //   );
   $(".menu-btn").on("click", function () {
     $(".menu ul").slideToggle();
   });
